a0c017a34dc203389fc679272c949d4c88384d18
repoe2e <professores@e2etreinamentos.com.br>
2024-12-05 18:19:02 -0300

    ok

--- src/test/java/restassured/core/ExtentManager.java
+++ src/test/java/restassured/core/ExtentManager.java
@@ -5,20 +5,21 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentManager {
 
-	  private static ExtentReports extent;
+    private static ExtentReports extent;
 
-	    public static ExtentReports getExtentReports() {
-	        if (extent == null) {
-	            ExtentSparkReporter sparkReporter = new ExtentSparkReporter("target/ExtentReport.html");
-	            sparkReporter.config().setDocumentTitle("API Test Report");
-	            sparkReporter.config().setReportName("RestAssured API Test Execution");
-	            sparkReporter.config().setTheme(com.aventstack.extentreports.reporter.configuration.Theme.STANDARD);
+    public static ExtentReports getExtentReports() {
+        if (extent == null) {
+            // Altere o caminho do relatório para "C:\statusreport"
+            ExtentSparkReporter sparkReporter = new ExtentSparkReporter("C:\\statusreport\\ExtentReport.html");
+            sparkReporter.config().setDocumentTitle("API Test Report");
+            sparkReporter.config().setReportName("RestAssured API Test Execution");
+            sparkReporter.config().setTheme(com.aventstack.extentreports.reporter.configuration.Theme.STANDARD);
 
-	            extent = new ExtentReports();
-	            extent.attachReporter(sparkReporter);
-	            extent.setSystemInfo("Environment", "Localhost");
-	            extent.setSystemInfo("Tester", "Your Name");
-	        }
-	        return extent;
-	    }
-	}
+            extent = new ExtentReports();
+            extent.attachReporter(sparkReporter);
+            extent.setSystemInfo("Environment", "Localhost");
+            extent.setSystemInfo("Tester", "Your Name");
+        }
+        return extent;
+    }
+}
